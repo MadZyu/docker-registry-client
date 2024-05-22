d62ef652bd57fb9e0e793e5a028a6ef7f720a422
zhangyu1440 <zhangyu1440@jd.com>
2024-05-22 13:16:26 +0800

    feat: catalog支持pagination

--- registry/repositories.go
+++ registry/repositories.go
@@ -5,11 +5,12 @@ type repositoriesResponse struct {
 }
 
 func (registry *Registry) Repositories() ([]string, error) {
-	url := registry.url("/v2/_catalog")
+	url := "/v2/_catalog?n=10"
 	repos := make([]string, 0, 10)
 	var err error //We create this here, otherwise url will be rescoped with :=
 	var response repositoriesResponse
 	for {
+		url = registry.URL + url
 		registry.Logf("registry.repositories url=%s", url)
 		url, err = registry.getPaginatedJSON(url, &response)
 		switch err {
